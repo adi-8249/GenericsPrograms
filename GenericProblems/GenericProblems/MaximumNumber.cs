69dc417631131d7b37e64d1f5a9a683f19de1e6d
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-20 21:22:29 +0530

    [Anjaneya]Added:Welcome message for generic

---
+++ GenericProblems/GenericProblems/MaximumNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericProblems
+{
+    public class MaximumNumber
+    {
+        public static void ToPrint(int[] inputArray)
+        {
+            foreach(int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-----------------");
+        }
+        public static void ToPrint(double[] inputArray)
+        {
+            foreach(double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-----------------");
+        }
+        public static void ToPrint(char[] inputArray)
+        {
+            foreach(char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------------");
+        }
+    }
+}
---
+++ GenericProblems/GenericProblems/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to generic programs");
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 1.2, 1.3, 1.4 };
+            char[] charArray = { 'H', 'E', 'F', 'P', 'S' };
+
+            MaximumNumber.ToPrint(intArray);
+            MaximumNumber.ToPrint(doubleArray);
+            MaximumNumber.ToPrint(charArray);
+        }
+    }
+}

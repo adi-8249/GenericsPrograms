80de80d4e33a7a26ae9f93ee65a02747c9935a01
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-20 23:11:41 +0530

    [Anjaneya]Added: Floats Number and test cases

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -9,6 +9,7 @@ namespace GenericProblems
             Console.WriteLine("Welcome to generic programs");
             MaximumNumber max = new MaximumNumber();
             Console.WriteLine(max.MaximumIntegerNumber(10,20,30));
+            Console.WriteLine(max.MaximumFloatNumber(10.5f,20.5f,30.5f));
         }
     }
 }
--- GenericProblems/MaxTest1.Test/UnitTest1.cs
+++ GenericProblems/MaxTest1.Test/UnitTest1.cs
@@ -11,6 +11,7 @@ namespace MaxTest1.Test
         }
 
         [Test]
+        //Testcase 1.1
         public void Maximum_Value_PassedAt_First_Parameter_Should_Return_Maximum_Value()
         {
             MaximumNumber max = new MaximumNumber();
@@ -21,6 +22,7 @@ namespace MaxTest1.Test
         }
 
         [Test]
+        //TestCase 1.2
         public void Maximum_Value_PassedAt_Second_Parameter_Should_Return_Maximum_Value()
         {
             MaximumNumber max = new MaximumNumber();
@@ -30,12 +32,44 @@ namespace MaxTest1.Test
             Assert.AreEqual(expected, actual);
         }
         [Test]
+        //TestCase 1.3
         public void Maximum_Value_PassedAt_Third_Parameter_Should_Return_Maximum_Value()
         {
             MaximumNumber find = new MaximumNumber();
             int firstValue = 20, secondValue = 10, thirdValue = 30;
             int expected = 30;
             int actual = find.MaximumIntegerNumber(firstValue, secondValue, thirdValue);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        //TestCase 2.1
+        public void Maximum_Float_Value_PassedAt_First_Parameter_Should_Return_Maximum_Value()
+        {
+            float firstValue = 30.5f, secondValue = 20.5f, thirdValue = 10.5f;
+            float expected = 30.5f;
+            MaximumNumber find = new MaximumNumber();
+            float actual = find.MaximumFloatNumber(firstValue, secondValue, thirdValue);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        //TestCase 2.2
+        public void Maximum_float_Value_PassedAt_Second_Parameter_Should_Return_Maximum_Value()
+        {
+            float firstValue = 20.5f, secondValue = 30.5f, thirdValue = 10.5f;
+            float expected = 30.5f;
+            MaximumNumber find = new MaximumNumber();
+            float actual = find.MaximumFloatNumber(firstValue, secondValue, thirdValue);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        //TestCase 2.3
+        public void Maximum_Float_Value_PassedAt_Third_Parameter_Should_Return_Maximum_Value()
+        {
+            float firstValue = 20.5f, secondValue = 10.5f, thirdValue = 30.5f;
+            float expected = 30.5f;
+            MaximumNumber find = new MaximumNumber();
+            float actual = find.MaximumFloatNumber(firstValue, secondValue, thirdValue);
             Assert.AreEqual(expected, actual);
         }
     }
